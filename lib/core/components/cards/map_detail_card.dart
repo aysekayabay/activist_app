9d6e49b4d3b5703434f351ea2c8dbfd470fd41f2
Berkan Yukselgun <berkanyukselgun@gmail.com>
2023-07-12 18:25:26 +0300

    Map Card UI Update

--- lib/core/components/cards/map_detail_card.dart
+++ lib/core/components/cards/map_detail_card.dart
@@ -13,26 +13,41 @@ class MapDetailCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 50),
-      decoration: BoxDecoration(color: AppColors.vanillaShake, borderRadius: BorderRadius.all(Radius.circular(55))),
+      decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.all(Radius.circular(20))),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          cachedNetworkImageWidget(posterUrl: event.posterUrl, height: 120, borderRadius: BorderRadius.vertical(top: Radius.circular(55))),
+          cachedNetworkImageWidget(
+              posterUrl: event.posterUrl,
+              height: 120,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+          SizedBox(height: 10),
           Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               Text(
                 event.name ?? '',
                 overflow: TextOverflow.ellipsis,
-                style: TextStyle(color: AppColors.black),
+                style: Theme.of(context).textTheme.titleLarge,
                 maxLines: 1,
               ),
-              Text(event.venue?.name ?? '', maxLines: 1, overflow: TextOverflow.ellipsis, style: TextStyle(color: AppColors.black)),
+              SizedBox(
+                height: 5,
+              ),
+              Text(event.venue?.name ?? '',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.labelMedium)
             ],
           ),
+          SizedBox(height: 10),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
               onPressed: () {
                 Navigator.of(context).push(MaterialPageRoute(
                   builder: (context) {
@@ -41,8 +56,11 @@ class MapDetailCard extends StatelessWidget {
                 ));
               },
               elevation: 6,
-              color: AppColors.orange,
-              child: Text("İncele"),
+              color: AppColors.grey,
+              child: Text(
+                "İncele",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
             ),
           )
         ],
